ed2cfab09df3051adab73b2e6de7d1f361609dd1
Christoph S. Ackermann <info@acki.be>
2011-04-06 09:27:27 +0200

    Added tools, added static function throwError

---
+++ _env/class/Tools.php
@@ -0,0 +1,17 @@
+<?php
+
+	class Tools {
+
+		public static function throwError($class, $function, $file, $line) {
+			print "Error!!<br /><br />
+			Class: ".$class."<br />
+			Method: ".$function."<br />
+			File: ".$file."<br />
+			Line: ".$line."<br />
+			";
+			exit;
+		}
+	
+	} //class
+	
+?>
