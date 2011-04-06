3332bfb5328b61178b5f4a2d894b70a87d1ef9ef
Christoph S. Ackermann <info@acki.be>
2011-04-06 22:00:54 +0200

    Created construct, implemented very simple getFullName

--- _env/class/Member.php
+++ _env/class/Member.php
@@ -1,11 +1,20 @@
 <?php
 
 	class Member {
-
-		public function __construct() {
-		
-		}
 	
+		protected 	$mysqli;
+		protected 	$id;
+
+		public function __construct($mysqli, $id=false) {
+			$this->id 		= $id;
+			$this->mysqli 	= $mysqli;
+		}//function construct
+		
+		public function getFullName() {
+			$value = $this->mysqli->select('member',array(id=>$this->id),'prename,name');
+			return $value[0]['prename'] . ' ' . $value[0]['name'];
+		}//function getName
+		
 	} //class
 	
 ?>
