f21e1073fa93089fc468c163734adfae0c8ece24
Christoph S. Ackermann <info@acki.be>
2011-04-06 09:27:02 +0200

    Worked on database class

--- _env/class/Database.php
+++ _env/class/Database.php
@@ -2,6 +2,54 @@
 
 	class Database extends mysqli {
 	
+		public 	$mysqli;
+		protected 	$toSelect;
+		protected 	$table;
+		protected 	$whereClauses;
+		private 	$query;
+	
+		public function setConnector($mysqli) {
+			$this->mysqli = $mysqli;
+		} //construct
+		
+		public function select($toSelect, $table, $whereClauses=false) {
+			$this->toSelect 	= $toSelect;
+			$this->table 		= $table;
+			$this->whereClauses = $whereClauses;
+			$where				= '';
+			
+			if(is_array($this->toSelect)) {
+				$this->toSelect = implode(',', $this->toSelect);
+			} //if is_array
+			
+			if($this->whereClauses) {
+				if(!is_array($this->whereClauses)) {
+					$first = true;
+					$where = ' WHERE ';
+					foreach($this->whereClauses as $val) {
+						if(!$first) {
+							$where .= ' AND ';
+						} else {
+							$first = false;
+						} //else
+						$where .= $val;
+					} //foreach
+				} //if is_array
+				else {
+					Tools::throwError(__CLASS__, __FUNCTION__, __FILE__, __LINE__);
+				}
+			} //if whereclauses
+			
+			$this->query		= 'SELECT ' . $this->toSelect . ' FROM ' . $this->table . $where;
+			
+			print $this->query;
+			
+		} //function select
+		
+		public function escape($string) {
+			return mysqli_real_escape_string($this->mysqli, $string);
+		} // function escape
+	
 	} //class
 	
 ?>
