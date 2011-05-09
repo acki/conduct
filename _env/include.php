d014c79bffc653c89713e5d6d98d969e28eacaa7
Christoph S. Ackermann <info@acki.be>
2011-05-09 09:01:55 +0200

    Added some algorithm for different configuration files

--- _env/include.php
+++ _env/include.php
@@ -10,20 +10,28 @@
 							'localhost',
 							'127.0.0.1',
 						);
-						
-	$found = false;
-	include('config.php');
-	foreach($development as $d) {
-		if ($_SERVER['HTTP_HOST'] == $d) {
-			include('config.development.php');
-			$found = true;
-		} //if
-	} // foreach
 	
-	if(!$found) {
-		include('config.production.php');
+	if (isset($_COOKIE['env']) && !isset($_GET['env'])) {
+		include('config.' . $_COOKIE['env'] . '.php');
+	} elseif (isset($_GET['env'])) {
+		setcookie('env', $_GET['env'], 0);
+		include('config.' . $_GET['env'] . '.php');
+	} else {
+		$found = false;
+		include('config.php');
+		foreach($development as $d) {
+			if ($_SERVER['HTTP_HOST'] == $d) {
+				include('config.development.php');
+				$found = true;
+			} //if
+		} // foreach
+		
+		if(!isset($found)) {
+			include('config.production.php');
+		}
 	}
-	
+		
+					
 	$mysqli = new Database($db['host'],$db['user'],$db['pass'],$db['db']);
 
 ?>
