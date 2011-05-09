3ae45db77b86aecba76afda3ccb1aa61a30b2ef0
Christoph S. Ackermann <info@acki.be>
2011-05-09 16:37:37 +0200

    Added a login form with auth verification

--- index.php
+++ index.php
@@ -14,4 +14,23 @@
 	$member = new Member($mysqli, $id);
 	print $member->getFullName();
 
+	if(!Login::isAuthed()) {
+
+		print '
+		<p>
+			<h1>Login</h1>
+			<form action="login/login.php" method="post">
+				<h3>Username: <input type="text" name="username"></h3>
+				<h3>Password: <input type="password" name="password"></h3>
+				<input type="submit" value="login">
+			</form>
+		</p>
+		';
+	
+	} else {
+		
+		print '<p><a href="login/logout.php">logout user '.$_SESSION['user_id'].'</a></p>';
+		
+	}
+
 ?>
